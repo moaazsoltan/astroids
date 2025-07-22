073bee9197e30f50f4fc18e4790005c9d516cb59
adam <sm885367@gmail.com>
2025-07-22 14:31:58 +0300

    refactors code to make sure things work as intended

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ def main():
     print(f"Screen width:", SCREEN_WIDTH)
     print(f"Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (000,000,000))
+        # Fill screen with black
+        screen.fill((0,0,0))
+
+        # Update game logic here
+        
+        # update the display
         pygame.display.flip()
+
+        # cap the frame and get delta time in Asteroids
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
